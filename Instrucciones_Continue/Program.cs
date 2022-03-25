b25a4b68807c17fc2eb258bddb91cf584cf7cc62
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:02:00 -0600

    Agregar archivos de proyecto.

---
+++ Instrucciones_Continue/Program.cs
@@ -0,0 +1,12 @@
+﻿//Continue
+for (int id = 0; id <= 10; id++)
+{
+    Thread.Sleep(800);
+    if (id == 8)
+    {
+        Console.WriteLine();
+        continue;
+    }
+    Console.WriteLine(id);
+}
+Console.ReadKey(true);
